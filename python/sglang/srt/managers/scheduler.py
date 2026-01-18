c20de1474904003382d67ea1ad7669b93e229e5d
Lancer <maruixiang6688@gmail.com>
2026-01-18 18:09:03 +0800

    [DLLM] support step map
    Signed-off-by: Lancer <maruixiang6688@gmail.com>

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -1513,6 +1513,7 @@ class Scheduler(
                 require_reasoning=recv_req.require_reasoning,
                 return_hidden_states=recv_req.return_hidden_states,
                 return_routed_experts=recv_req.return_routed_experts,
+                return_step_maps=recv_req.return_step_maps,
                 eos_token_ids=self.model_config.hf_eos_token_id,
                 bootstrap_host=recv_req.bootstrap_host,
                 bootstrap_port=recv_req.bootstrap_port,
